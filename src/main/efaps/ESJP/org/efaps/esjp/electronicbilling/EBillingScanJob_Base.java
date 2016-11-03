df938adb715552a9a833c9bed50ff0f36d6d07a2
Jan Moxter <jan.moxter@innobix.com>
2016-11-03 14:23:41 -0500

    - Issue #8: Add esjp to be used by Quartz
    closes #8

---
+++ src/main/efaps/ESJP/org/efaps/esjp/electronicbilling/EBillingScanJob_Base.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2003 - 2016 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.efaps.esjp.electronicbilling;
+
+import org.efaps.admin.event.Parameter;
+import org.efaps.admin.program.esjp.EFapsApplication;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.admin.user.Company;
+import org.efaps.ci.CIAdminUser;
+import org.efaps.db.Context;
+import org.efaps.db.InstanceQuery;
+import org.efaps.db.QueryBuilder;
+import org.efaps.util.EFapsException;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The Class EBillingScanJob_Base.
+ *
+ * @author The eFaps Team
+ */
+@EFapsUUID("8a464cb3-4b8c-4530-82f2-7da1c1e8813b")
+@EFapsApplication("eFapsApp-ElectronicBilling")
+public abstract class EBillingScanJob_Base
+    implements Job
+{
+    /**
+     * Logging instance used in this class.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(EBillingScanJob.class);
+
+    @Override
+    public void execute(final JobExecutionContext _context)
+        throws JobExecutionException
+    {
+
+        try {
+            final QueryBuilder queryBldr = new QueryBuilder(CIAdminUser.Company);
+            final InstanceQuery query = queryBldr.getQuery();
+            query.executeWithoutAccessCheck();
+            while (query.next()) {
+                final Company company = Company.get(query.getCurrentValue().getId());
+                Context.getThreadContext().setCompany(company);
+                final Parameter parameter = new Parameter();
+                new EBillingDocument().scan4Documents(parameter);
+            }
+            // remove the company to be sure
+            Context.getThreadContext().setCompany(null);
+        } catch (final EFapsException e) {
+            EBillingScanJob_Base.LOG.error("Catched error", e);
+        }
+    }
+}
